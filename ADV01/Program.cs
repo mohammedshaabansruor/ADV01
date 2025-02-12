6ede711c27dbd4c97bd584434c29dcdf60bb8600
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-02-12 18:54:26 +0200

    q2

--- ADV01/Program.cs
+++ ADV01/Program.cs
@@ -5,13 +5,13 @@
         static void Main(string[] args)
         {
 
-            #region Q1
-            Range<int> range = new Range<int>(1, 10);
-            Console.WriteLine(range.IsInRange(5)); // True
-            Console.WriteLine(range.IsInRange(15)); // False
-            Console.WriteLine(range.Length());
-
+            #region Q02
+            int[] Numbers = { 1, 2, 3, 4, 5 };
+            Helper<int>.Reverse(Numbers);
             #endregion
+
+
+
         }
     }
 }
