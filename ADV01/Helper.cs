1b9776495ee3efa41c31a1843b53938642c06136
mohammedshaabansruor <shaabanmohammed490@gmail.com>
2025-02-12 18:53:02 +0200

    q1

---
+++ ADV01/Helper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV01
+{
+    internal static class Helper
+    {
+        public static void Reverse<T>(T[] array)
+        {
+           
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+
+
+        }
+
+        public static void GetOddNumbers(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Console.Write($"{array[i]}, ");
+                }
+
+            }
+        }
+
+        public static void FirstNonRepeatedCharacter(string stir)
+        {
+            if (string.IsNullOrEmpty(stir))
+            {
+                Console.WriteLine("Input string cannot be null or empty.");
+                return;
+            }
+            Dictionary<char, int> chars = new Dictionary<char, int>();
+            for (int i = 0; i < stir.Length; i++)
+            {
+                if (!chars.ContainsKey(stir[i])) chars.Add(stir[i], 1);
+                else chars[stir[i]]++;
+            }
+            bool flage = false;
+            foreach (char a in stir)
+            {
+                if (chars[a] == 1)
+                {
+                    Console.WriteLine($"the Character is {a}");
+                    flage = true;
+                    return;
+                }
+            }
+            if (!flage)
+                Console.WriteLine("No non-repeated character found");
+        }
+    }
+}
--- ADV01/Program.cs
+++ ADV01/Program.cs
@@ -4,37 +4,14 @@
     {
         static void Main(string[] args)
         {
+
             #region Q1
             Range<int> range = new Range<int>(1, 10);
-            Console.WriteLine(range.IsInRange(5)); 
-            Console.WriteLine(range.IsInRange(15)); 
+            Console.WriteLine(range.IsInRange(5)); // True
+            Console.WriteLine(range.IsInRange(15)); // False
             Console.WriteLine(range.Length());
 
             #endregion
-            #region Q02
-            int[] Numbers = { 1, 2, 3, 4, 5 };
-            Helper<int>.Reverse(Numbers);
-            #endregion
-
-            #region Q03
-            int[] Numbers = { 1, 2, 3, 4, 5 };
-            Helper.GetOddNumbers(Numbers);
-            #endregion
-
-
-            #region Q04
-            FixedSizeList<int> numbers = new FixedSizeList<int>(3);
-
-            numbers.Add(10);
-            numbers.Add(20);
-            numbers.Add(30);
-            numbers.Add(40);
-            #endregion
-
-            
-
-            
-          
         }
     }
 }
